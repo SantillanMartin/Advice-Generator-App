97ac9663e97dd7f74c32da82f973af63bd1b5dbf
SantillanMartin <martinsantillan.a1@gmail.com>
2022-11-25 00:47:24 -0300

    first commit/created index html/index js/added styles/added api

---
+++ index.js
@@ -0,0 +1,28 @@
+let contenedorAviso=document.getElementById("contenedor-aviso");
+let mensaje=document.getElementById("mensaje");
+let boton=document.getElementById("boton-aviso");
+let avisoId=document.getElementById("id-aviso");
+
+boton.onclick=()=>{
+    MostrarAviso();
+}
+
+
+
+function MostrarAviso(){
+    fetch("https://api.adviceslip.com/advice")
+.then(res => res.json() )
+.then(data => {
+    //const AVISO= data.slip;
+    mensaje.innerHTML=`"${data.slip.advice}"`;
+    avisoId.innerHTML=`ADVICE #${data.slip.id}`;
+    
+ } );
+
+
+}
+
+
+
+
+
